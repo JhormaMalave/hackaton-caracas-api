79fa33f685de38a50fc63ef4c23cd9f84baf938f
JhormaMalave <jhormamalave99@gmail.com>
2022-02-01 22:45:23 -0400

    Configure JWT to project

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::UsersController < ApplicationController
+    #skip_before_action :authenticate_request
+
     def index
         @users = User.all
         render json: @users
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::API
+    include JsonWebToken
+
+    before_action :authenticate_request
+
+    private
+        def authenticate_request
+            header = request.headers["Authorization"]
+            
+            if !header
+                render json: { error: 'Debes iniciar sesión' }, status: 401
+            else 
+                header = header.split(" ").last if header
+                decoded = jwt_decode(header)
+                @current_user = User.find(decoded[:user_id])
+            end
+        end
 end
