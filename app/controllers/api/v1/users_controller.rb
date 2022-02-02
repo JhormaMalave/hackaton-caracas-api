e7b9a451e59bf3e0658888c4120eb9f09e12b421
JhormaMalave <jhormamalave99@gmail.com>
2022-02-01 22:58:54 -0400

    Generate sing in action

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,8 +1,24 @@
 class Api::V1::UsersController < ApplicationController
-    #skip_before_action :authenticate_request
+    skip_before_action :authenticate_request, only: :create
 
     def index
         @users = User.all
         render json: @users
     end
+
+    # POST /users
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            render json: @user, status: :created
+        else
+            render json: { error: 'No se pudo crear el usuario.' }, status: :unprocessable_entity
+        end 
+    end
+
+    private
+        def user_params
+            params.permit(:email, :username, :password)
+        end
+      
 end
