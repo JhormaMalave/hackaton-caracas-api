14cfbb5d1b4e11ab76d449d34d5e858bdab29a6b
JhormaMalave <jhormamalave99@gmail.com>
2022-02-02 11:23:24 -0400

    Add actions to medical items controller

--- app/controllers/api/v1/medical_items_controller.rb
+++ app/controllers/api/v1/medical_items_controller.rb
@@ -1,2 +1,51 @@
 class Api::V1::MedicalItemsController < ApplicationController
+  before_action :set_medical_item, only: %i[ show update destroy ]
+
+  # GET /medical_items
+  def index
+    @medical_items = MedicalItem.all
+
+    render json: @medical_items
+  end
+
+  # GET /medical_items/1
+  def show
+    render json: @medical_item
+  end
+
+  # POST /medical_items
+  def create
+    @medical_item = MedicalItem.new(medical_item_params)
+
+    if @medical_item.save
+      render json: @medical_item, status: :created, location: @medical_item
+    else
+      render json: @medical_item.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /medical_items/1
+  def update
+    if @medical_item.update(medical_item_params)
+      render json: @medical_item
+    else
+      render json: @medical_item.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /medical_items/1
+  def destroy
+    @medical_item.destroy
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_medical_item
+      @medical_item = MedicalItem.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def medical_item_params
+      params.require(:medical_item).permit(:name)
+    end
 end
